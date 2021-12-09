9c7fa8909c113b87b0a4c30cea01e1fb17a1c4e9
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-12-09 13:32:22 +0100

    Add ext_tables.sql

---
+++ ext_tables.sql
@@ -0,0 +1,12 @@
+#
+# Table structure for table 'tx_slubfindflexibleviews_domain_model_flexibleviews'
+#
+CREATE TABLE tx_slubfindflexibleviews_domain_model_flexibleviews (
+
+	title varchar(255) DEFAULT '' NOT NULL,
+	query varchar(255) DEFAULT '' NOT NULL,
+	description varchar(255) DEFAULT '' NOT NULL,
+	image int(11) unsigned NOT NULL default '0',
+	link varchar(255) DEFAULT '' NOT NULL
+
+);
