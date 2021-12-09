b9c98eca35b6b0415ba28736111fb2334f6f65d7
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-12-09 10:25:21 +0100

    add initial files

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Find Flexible Views for SLUB Catalog',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Alexander Bigga',
+	'author_email' => 'typo3@slub-dresden.de',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+            'typo3' => '9.5.0-9.5.99'
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,25 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+/** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
+$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
+
+// register routing configs
+// $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['SlubCatalogId'] = \Slub\SlubFindExtend\Routing\Aspect\SlubCatalogId::class;
+
+// Hook into \Subugoe\Find\Controller
+$signalSlotDispatcher->connect(
+    'Subugoe\Find\Controller\SearchController',
+    'indexActionBeforeSelect',
+    'Slub\SlubFindExtend\Slots\AdvancedQuery',
+    'build',
+    false
+);
+
+$cacheKey = 'find_flexibleviews';
+if( !is_array( $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ] ) ){
+    $cacheConfig =  $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ] = array();
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ]['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ]['backend'] = 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend';
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations'][ $cacheKey ]['options'] = array();
+}
