3ee32d0855c78fa71c80c45b6675d06d204a3881
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2021-12-10 14:13:38 +0100

    Add empty argument test

---
+++ Tests/Unit/Slots/AdvancedQueryTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Slub\SlubFindFlexibleviews\Tests\Unit\Slots;
+
+use Solarium\QueryType\Select\Query\Query;
+use Slub\SlubFindFlexibleviews\Slots\AdvancedQuery;
+
+class AdvancedQueryTest extends \TYPO3\TestingFramework\Core\Unit\UnitTestCase {
+
+    /**
+     * @var AdvancedQuery
+     */
+    protected $advancedQuery = null;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->advancedQuery = new AdvancedQuery();
+    }
+
+    /**
+     * @test
+     */
+    public function nullArgumentsDontChangeQuery() {
+        $preparedQuery = new Query();
+        $preparedQueryString = $preparedQuery->getQuery();
+
+        $this->advancedQuery->build($preparedQuery, null);
+
+        $this->assertEquals($preparedQuery->getQuery(), $preparedQueryString);
+    }
+
+}
