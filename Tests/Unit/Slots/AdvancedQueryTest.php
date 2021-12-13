811db5d1bfb6096a7274ce33dcda8be578566135
Matthias Richter <matthias@localhost.localdomain>
2021-12-13 17:13:12 +0100

    Add Function Tests

--- Tests/Unit/Slots/AdvancedQueryTest.php
+++ Tests/Unit/Slots/AdvancedQueryTest.php
@@ -3,6 +3,8 @@ namespace Slub\SlubFindFlexibleviews\Tests\Unit\Slots;
 
 use Solarium\QueryType\Select\Query\Query;
 use Slub\SlubFindFlexibleviews\Slots\AdvancedQuery;
+use Slub\SlubFindFlexibleviews\Domain\Repository\FlexibleviewsRepository;
+use Slub\SlubFindFlexibleviews\Domain\Model\Flexibleviews;
 
 class AdvancedQueryTest extends \TYPO3\TestingFramework\Core\Unit\UnitTestCase {
 
@@ -11,10 +13,31 @@ class AdvancedQueryTest extends \TYPO3\TestingFramework\Core\Unit\UnitTestCase {
      */
     protected $advancedQuery = null;
 
+    /**
+     * @var FlexibleviewsRepository
+     */
+    protected $flexibleviewsRepository = null;
+
     protected function setUp()
     {
         parent::setUp();
-        $this->advancedQuery = new AdvancedQuery();
+        // Mock is necessary for being able to inject mocked version of flexibleviewsRepository
+        $this->advancedQuery = $this->getMockBuilder(AdvancedQuery::class)
+            ->setMethodsExcept(['build', 'injectFlexibleviewsRepository'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->flexibleviewsRepository = $this->getMockBuilder(FlexibleviewsRepository::class)
+            ->setMethods(['findByUid'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $flexibleviews = new Flexibleviews();
+        $this->flexibleviewsRepository
+             ->method('findByUid')
+             ->will($this->returnValueMap([[1, $flexibleviews]]));
+
+        $this->inject($this->advancedQuery, 'flexibleviewsRepository', $this->flexibleviewsRepository);
     }
 
     /**
@@ -27,6 +50,7 @@ class AdvancedQueryTest extends \TYPO3\TestingFramework\Core\Unit\UnitTestCase {
         $this->advancedQuery->build($preparedQuery, null);
 
         $this->assertEquals($preparedQuery->getQuery(), $preparedQueryString);
+        $this->assertEquals(count($preparedQuery->getFilterQueries()), 0);
     }
 
     /**
@@ -38,7 +62,37 @@ class AdvancedQueryTest extends \TYPO3\TestingFramework\Core\Unit\UnitTestCase {
 
         $this->advancedQuery->build($preparedQuery, []);
 
+        $this->assertEquals(count($preparedQuery->getFilterQueries()), 0);
         $this->assertEquals($preparedQuery->getQuery(), $preparedQueryString);
+    }
+
+    /**
+     * @test
+     */
+    public function argumentIsAppendedToQuery() {
+        $preparedQuery = new Query();
+        $arguments = array(
+            'flexibleviews' => '1' 
+        );
+        $countFilterQueries = count($preparedQuery->getFilterQueries());
+
+        $this->advancedQuery->build($preparedQuery, $arguments);
+
+        $this->assertEquals(count($preparedQuery->getFilterQueries()), $countFilterQueries + 1);
+    }
+
+    /**
+     * @test
+     */
+    public function invalidArgumentIsNotAppendedToQuery() {
+        $preparedQuery = new Query();
+        $arguments = array(
+            'flexibleviews' => '2' 
+        );
+
+        $this->advancedQuery->build($preparedQuery, $arguments);
+
+        $this->assertEquals(count($preparedQuery->getFilterQueries()), 0);
     }
 
 }
